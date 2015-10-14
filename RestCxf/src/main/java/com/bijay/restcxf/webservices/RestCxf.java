5e8f7340df06b7c8ec36f9146145486ebe492134
erbijay <java.bhandari85@gmail.com>
2015-10-14 10:59:48 -0500

    second commit

--- RestCxf/src/main/java/com/bijay/restcxf/webservices/RestCxf.java
+++ RestCxf/src/main/java/com/bijay/restcxf/webservices/RestCxf.java
@@ -5,6 +5,8 @@ public class RestCxf {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I am using rest cxf");
+		
+		System.out.println("This is second commit.02");
 
 	}
 
