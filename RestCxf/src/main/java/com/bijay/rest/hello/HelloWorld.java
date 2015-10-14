5018f7692d25c29b3a090a92ba41b556c94b1f32
erbijay <java.bhandari85@gmail.com>
2015-10-14 09:40:08 -0500

    Initial phase setup

---
+++ RestCxf/src/main/java/com/bijay/rest/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.bijay.rest.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi i am here with Maven-RestCxf");
+
+	}
+
+}
