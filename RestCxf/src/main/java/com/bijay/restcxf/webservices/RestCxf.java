38247a64e82ffd02ed84d9908f69501dcf420562
erbijay <java.bhandari85@gmail.com>
2015-10-14 10:50:00 -0500

    Intial rest cxf setup...

---
+++ RestCxf/src/main/java/com/bijay/restcxf/webservices/RestCxf.java
@@ -0,0 +1,11 @@
+package com.bijay.restcxf.webservices;
+
+public class RestCxf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am using rest cxf");
+
+	}
+
+}
