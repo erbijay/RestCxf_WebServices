f82b17d62069d6245b061640b8f4ee7551f9f343
erbijay <java.bhandari85@gmail.com>
2015-10-14 10:05:14 -0500

    testing code for second time...

--- RestCxf/src/main/java/com/bijay/rest/hello/HelloWorld.java
+++ RestCxf/src/main/java/com/bijay/rest/hello/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi i am here with Maven-RestCxf");
+		System.out.println("this is second test now 02");
 
 	}
 
