c2fdc3ce1925465636f3cc6f64c8c8c892d8c9c5
erbijay <java.bhandari85@gmail.com>
2015-10-14 11:12:55 -0500

    class customer changes with name and phone...

--- RestCxf/src/main/java/com/bijay/restcxf/model/Customer.java
+++ RestCxf/src/main/java/com/bijay/restcxf/model/Customer.java
@@ -2,9 +2,20 @@ package com.bijay.restcxf.model;
 
 public class Customer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public String name;
+	public String phone;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 
 }
